383f7e974a1de6d111e2d24b11ec44e70553a017
Willy <willykipchumba22@gmail.com>
2023-10-04 16:56:04 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(myname, myemail) {
+  fetch(`http://localhost:3000/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: myname,
+      email: myemail,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("span2").textContent = data.id;
+      document.getElementById("span").textContent = "Unauthorized Access";
+    })
+    .catch((error) => {
+      console.error("Unauthorized Access", error);
+      document.getElementById("span").textContent = "Unauthorized Access";
+    });
+}
